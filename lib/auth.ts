6c2f4bcfea1c1d0c675f108128f71652f7f677e0
Arinze-Eze <emmanuelrinz@gmail.com>
2025-07-04 01:26:58 +0100

    chore: update dependencies and add new packages
    - Added @neondatabase/serverless, dotenv, drizzle-orm, drizzle-kit, and tsx to dependencies.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,22 +1,23 @@
 import { betterAuth } from "better-auth";
 import { magicLink } from "better-auth/plugins";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  //...
-  rateLimit: {
-    window: 50, // time window in seconds
-    max: 10, // max requests in the window
-  },
   socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    },
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  rateLimit: {
+    window: 60, // time window in seconds
+    max: 10, // max requests in the window
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
   plugins: [
     magicLink({
       sendMagicLink: async ({ email, token, url }) => {
@@ -24,5 +25,6 @@ export const auth = betterAuth({
         console.log(`Magic link URL: ${url}`);
       },
     }),
+    nextCookies(),
   ],
 });
