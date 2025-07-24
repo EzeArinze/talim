7b7dd6277efb607dc78d6bcfcc579c7d4e0a5438
Arinze-Eze <emmanuelrinz@gmail.com>
2025-07-24 15:49:49 +0100

    file types

---
+++ types/file-upload-state.d.ts
@@ -0,0 +1,11 @@
+interface UploaderProps {
+  id: string | null;
+  file: File | null;
+  uploading?: boolean;
+  progress?: number;
+  key?: string;
+  isDeleting: boolean;
+  error: boolean;
+  objectUrl?: string;
+  fileType: "image" | "video";
+}
